f9a7b93631a9237a9f204bbf39be48a82df4650c
RemiIO Scarlet <i@remi.email>
2022-10-11 23:29:09 +0800

    Add react dependents for metamask.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { MetaMaskProvider } from "metamask-react";
+import { defaultDarkTheme } from "../utils/theme";
+import { ThemeProvider } from "@mui/material/styles";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={defaultDarkTheme}>
+      <MetaMaskProvider>
+        <Component {...pageProps} />
+      </MetaMaskProvider>
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
